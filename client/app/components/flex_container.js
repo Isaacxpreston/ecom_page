4bc5cf86717b35b1b079a67b3a99f25800ce3350
Isaac <isaacxpreston@gmail.com>
2017-02-28 17:49:24 -0800

    initial commit

---
+++ client/app/components/flex_container.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import './flex_container_styles.css'
+
+const FlexContainer = React.createClass({
+
+	render() {
+    return (
+      <div className="container">
+        <div>
+          yeehaw
+        </div>
+        <div>
+          lorem dick
+        </div>
+      </div>
+    )
+	}
+})
+
+export default FlexContainer;
---
+++ client/app/components/mainwrapper.js
@@ -0,0 +1,53 @@
+import React from 'react'
+import {Link, browserHistory} from 'react-router'
+import {connect} from 'react-redux'
+import FlexContainer from './flex_container'
+import '../stylesheets/stylesheet.css'
+
+const Main = React.createClass({
+
+	// someAction () {
+	// 	this.props.dispatch(someAction())
+	// },
+	
+	// signUp () {
+	// 	this.props.dispatch(signUp())
+	// },
+
+	//have three layers
+		//navbar
+		//content
+			//features
+			//pricing
+			//footer
+		//background image
+
+	//make a layer class that has flexbox properties and see if it works on children injected into it
+	render() {
+		return (
+			<div>
+
+				we gonna feed u all props blalbh
+				<div className="something">header nav</div>
+				<FlexContainer />
+				backgrond
+				features
+				pricing
+				foot
+
+
+
+			</div>
+		)
+	}
+})
+
+function mapStatetoProps (state=[]) {
+	return {
+		simpleReducer: state.simpleReducer,
+	}
+}
+
+const MainWrapper = connect(mapStatetoProps)(Main);
+
+export default MainWrapper;
